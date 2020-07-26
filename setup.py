4830b2c31f56b4a1b233340f90ccd9d5075e4b37
Uladzislau Vasilyeu <vasvlad@gmail.com>
2020-07-26 20:40:37 +0300

    Development proccess started

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="noolite2mqtt",
+    version="0.1.0",
+    author="Uladzislau Vasilyeu",
+    author_email="vasvlad@gmail.com",
+    description="NooLite to MQTT bridge",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vasvlad/noolite2mqqt",
+    keywords="noolite mqtt",
+    packages=['noolite2mqtt'],
+    install_requires=[
+        'NooLite_F',
+    ],
+    scripts=[
+    ],
+    entry_points={
+    },
+    classifiers=[
+        "Development Status :: 1 - Beta",
+        "Topic :: Home Automation",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
