cb0dfde3c98c1c7bf5b3ca26a1e73d9ae3f9bfa5
Uladzislau Vasilyeu <vasvlad@gmail.com>
2020-07-28 20:56:51 +0300

    after flake8

--- noolite2mqtt/noolite2mqtt.py
+++ noolite2mqtt/noolite2mqtt.py
@@ -2,18 +2,11 @@
 
 import argparse
 import time
-import NooLite_F
-from NooLite_F.MTRF64.MTRF64Adapter import MTRF64Adapter, IncomingData, OutgoingData, Command, Mode, Action, ResponseCode, IncomingDataException
-from NooLite_F.MTRF64.MTRF64Controller import MTRF64Controller
+from NooLite_F.MTRF64.MTRF64Adapter import MTRF64Adapter, IncomingData
 
 
 def on_receive_data(incoming_data: IncomingData):
     print("data: {0}".format(incoming_data))
-
-
-
-
-
 
 
 parser = argparse.ArgumentParser(description='NooLite to MQTT bridge')
